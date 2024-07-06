73e1867ee5ce11ed81374632fc9c7cf781d4e170
LIinz 27 <michaelunyu26@gmail.com>
2024-07-06 16:15:51 +0800

    UseHook Data & On Change

---
+++ useEffect.js
@@ -0,0 +1,39 @@
+import {Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+      }}
+    >
+      <Text>Daftar User</Text>
+      {users.map((user) => (
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: "black",
+            padding: 20,
+            margin: 5,
+          }}
+          key={user.id}
+        >
+          <Text>Nama: {user.name}</Text>
+          <Text>Username: {user.username}</Text>
+          <Text>Email: {user.email}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+export default App;
